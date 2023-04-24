d390c8f11914a0ad7e8e59cd497e476295a9dba3
Gary Ridgway <gary-ridgway@uiowa.edu>
2023-04-24 16:41:23 +0200

    Title not changing on examples.

--- src/js/docJs.js
+++ src/js/docJs.js
@@ -5,7 +5,7 @@
  */
 let globals = [];
 const regex = /global$/gi;
-document.querySelectorAll('h1, h2, h3, h4, h5, h6').forEach(function(element){
+document.querySelectorAll('h1, h2, h3, h4, h5, h6, title').forEach(function(element){
     const found = element.textContent.match(regex);
     if (found) {
         element.textContent = 'Examples';
